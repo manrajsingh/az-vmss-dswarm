69eee90915f058fdf1f00b384ce8491dbdb99b1a
Manraj Singh <manrajsingh84@gmail.com>
2020-10-19 01:01:01 -0700

    Update deb-dswarm.sh

--- deb-dswarm.sh
+++ deb-dswarm.sh
@@ -41,3 +41,5 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
 sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sudo usermod -aG docker manraj
